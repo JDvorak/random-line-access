546a21fc3335ae7774e6962c4183b04df74bc35e
John Dvorak <john@johnsdvorak.com>
2018-05-07 22:40:16 -0700

    Apply standard

--- test.js
+++ test.js
@@ -29,20 +29,20 @@ test('should retrieve lines by key defined in the first word', function (t) {
     "bafloon mango" george chai
   `))
   let rl = rla('test.txt')
-  rl.get('example',function(err, str) {
+  rl.get('example', function (err, str) {
     t.equal(str, '2 3 4 5 6')
-    rl.get('another',function(err, str) {
+    rl.get('another', function (err, str) {
       t.equal(str, '1 2 3 4 cat', 'failed to retrieve string with text')
-      rl.get('third',function(err, str) {
+      rl.get('third', function (err, str) {
         t.equal(str, 'mango pinata long string  another')
-        rl.get('"bafloon mango"',function(err, str) {
+        rl.get('"bafloon mango"', function (err, str) {
           t.error()
-          rl.ls(function(err, keys) {
+          rl.ls(function (err, keys) {
             t.deepEqual(keys, ['example', 'another', 'third', '"bafloon'])
             var newValue = 'hippy hooperango'
             rl.set('third', newValue, function (err, ok) {
               t.ok(ok)
-              rl.get('third', function(err, value) {
+              rl.get('third', function (err, value) {
                 t.equal(value.trim(), newValue)
               })
             })
@@ -53,7 +53,6 @@ test('should retrieve lines by key defined in the first word', function (t) {
   })
 })
 
-
 test('should retrieve lines from csv sets as well', function (t) {
   t.plan(7)
   fs.writeFileSync('test.txt', trim(`
@@ -63,20 +62,20 @@ test('should retrieve lines from csv sets as well', function (t) {
     "bafloon mango",george,chai,,,
   `))
   let rl = rla('test.txt', {sep: ',', quotes: true, omitEmpty: false})
-  rl.get('example',function(err, str) {
-    t.deepEqual(str, ['2', '3', '4', '5', '6',])
-    rl.get('another',function(err, str) {
+  rl.get('example', function (err, str) {
+    t.deepEqual(str, ['2', '3', '4', '5', '6'])
+    rl.get('another', function (err, str) {
       t.deepEqual(str, ['1', '2', '3', '4', 'cat'], 'failed to retrieve string with text')
-      rl.get('third',function(err, str) {
+      rl.get('third', function (err, str) {
         t.deepEqual(str, ['mango', 'pinata', 'long', 'string', '', 'another'])
-        rl.get('bafloon mango',function(err, str) {
+        rl.get('bafloon mango', function (err, str) {
           t.deepEqual(str, ['george', 'chai', '', '', ''])
-          rl.ls(function(err, keys) {
+          rl.ls(function (err, keys) {
             t.deepEqual(keys, ['example', 'another', 'third', 'bafloon mango'])
             var newValue = ['hippy', 'hooperango']
             rl.set('third', newValue, function (err, ok) {
               t.ok(ok)
-              rl.get('third', function(err, value) {
+              rl.get('third', function (err, value) {
                 t.deepEqual(value, newValue)
               })
             })
@@ -87,7 +86,6 @@ test('should retrieve lines from csv sets as well', function (t) {
   })
 })
 
-
 test('should retrieve lines by key defined in the first word, including those wrapped in quotes', function (t) {
   t.plan(7)
   fs.writeFileSync('test.txt', trim(`
@@ -97,20 +95,20 @@ test('should retrieve lines by key defined in the first word, including those wr
     "bafloon mango" george chai
   `))
   let rl = rla('test.txt', {quotes: true})
-  rl.get('example',function(err, str) {
+  rl.get('example', function (err, str) {
     t.equal(str, '2 3 4 5 6')
-    rl.get('another',function(err, str) {
+    rl.get('another', function (err, str) {
       t.equal(str, '1 2 3 4 cat', 'failed to retrieve string with text')
-      rl.get('third',function(err, str) {
+      rl.get('third', function (err, str) {
         t.equal(str, 'mango pinata long string  another')
-        rl.get('bafloon mango',function(err, str) {
+        rl.get('bafloon mango', function (err, str) {
           t.equal(str, 'george chai')
-          rl.ls(function(err, keys) {
+          rl.ls(function (err, keys) {
             t.deepEqual(keys, ['example', 'another', 'third', 'bafloon mango'])
             var newValue = 'hippy hooperango'
             rl.set('third', newValue, function (err, ok) {
               t.ok(ok)
-              rl.get('third', function(err, value) {
+              rl.get('third', function (err, value) {
                 t.equal(value.trim(), newValue)
               })
             })
@@ -130,20 +128,20 @@ test('should retrieve lines as space separated sets', function (t) {
     "bafloon mango" george chai
   `))
   let rl = rla('test.txt', {sep: ' ', quotes: true})
-  rl.get('example',function(err, str) {
-    t.deepEqual(str, ['2', '3', '4', '5', '6',])
-    rl.get('another',function(err, str) {
+  rl.get('example', function (err, str) {
+    t.deepEqual(str, ['2', '3', '4', '5', '6'])
+    rl.get('another', function (err, str) {
       t.deepEqual(str, ['1', '2', '3', '4', 'cat'], 'failed to retrieve string with text')
-      rl.get('third',function(err, str) {
+      rl.get('third', function (err, str) {
         t.deepEqual(str, ['mango', 'pinata', 'long', 'string', 'another'])
-        rl.get('bafloon mango',function(err, str) {
+        rl.get('bafloon mango', function (err, str) {
           t.deepEqual(str, ['george', 'chai'])
-          rl.ls(function(err, keys) {
+          rl.ls(function (err, keys) {
             t.deepEqual(keys, ['example', 'another', 'third', 'bafloon mango'])
             var newValue = ['hippy', 'hooperango']
             rl.set('third', newValue, function (err, ok) {
               t.ok(ok)
-              rl.get('third', function(err, value) {
+              rl.get('third', function (err, value) {
                 t.deepEqual(value, newValue)
               })
             })
